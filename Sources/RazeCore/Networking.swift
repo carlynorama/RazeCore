d1b725db159883a56f93ea04d88c133eceeffc40
carlynorama <carlynorama@gmail.com>
2020-09-29 12:08:23 -0700

    Added Networking Class

---
+++ Sources/RazeCore/Networking.swift
@@ -0,0 +1,14 @@
+//
+//  Networking.swift
+//  RazeCore
+//
+//  Created by Carlyn Maw on 9/29/20.
+//
+
+import Foundation
+
+extension RazeCore {
+    public class Networking {
+        
+    }
+}
--- Sources/RazeCore/RazeCore.swift
+++ Sources/RazeCore/RazeCore.swift
@@ -1,42 +1,12 @@
-//import UIKit
-//import Foundation
-import SwiftUI
+//
+//  RazeCore.swift
+//  RazeCore
+//
+//  Created by Carlyn Maw on 9/29/20.
+//
 
+import Foundation
 
-
-
-public class RazeCore {
+public struct RazeCore {
     
-    /// Allows you to convert a 6 digit hexadecimal string into a UIColor string
-    /// - Parameters:
-    ///     - Warning: The "#" symbol is stripped from the begining
-    ///     - hexString: expects 6 digit hex string in the format RRGGBB, with or without leading #
-    ///     - alpha: CGFloat for alpha, 0 to 1, default 1
-    /// - Returns: UIColor determined by `hexString`
-    internal class func colorFromHexString(_ hexString: String, alpha: CGFloat = 1.0) -> UIColor {
-      let r, g, b: CGFloat
-      let offset = hexString.hasPrefix("#") ? 1 : 0
-      let start = hexString.index(hexString.startIndex, offsetBy: offset)
-      let hexColor = String(hexString[start...])
-      let scanner = Scanner(string: hexColor)
-      var hexNumber: UInt64 = 0
-      if scanner.scanHexInt64(&hexNumber) {
-        r = CGFloat((hexNumber & 0xff0000) >> 16) / 255
-        g = CGFloat((hexNumber & 0x00ff00) >> 8) / 255
-        b = CGFloat(hexNumber & 0x0000ff) / 255
-        return UIColor(red: r, green: g, blue: b, alpha: alpha)
-      }
-      return UIColor(red: 0, green: 0, blue: 0, alpha: alpha)
-    }
-    
-    
-    /// The one true color.
-    public static var razeColor: UIColor {
-        return self.colorFromHexString("006736")
-    }
-    
-    /// Returns the second best UIColor in the world.
-    public static var secondaryRazeColor: UIColor {
-        return self.colorFromHexString("FCFFFD")
-    }
 }
